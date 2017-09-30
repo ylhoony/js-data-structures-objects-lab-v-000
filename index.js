e3883b8392fc21b7fb9350ffcbabd8a3f0f0ffb9
Hoon Lee <yl.hoony@gmail.com>
2017-09-30 19:18:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-  const newDriver = Object.assign(driver, {});
+  const newDriver = Object.assign({}, driver);
   delete newDriver[key];
   return newDriver;
 }
