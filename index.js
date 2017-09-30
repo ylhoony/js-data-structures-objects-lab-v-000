e58f3a1f8737d057349d432f8761313c9a0a895d
Hoon Lee <yl.hoony@gmail.com>
2017-09-30 19:15:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,21 +13,11 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return driver;
 }
 
+function deleteFromDriverByKey(driver, key) {
+  
+}
 
-//
-// describe('destructivelyUpdatedriverWithKeyAndValue(driver, key, value)', function () {
-//   it('updates `driver` with the given `key` and `value` (it is destructive) and returns the entire updated driver', function () {
-//     expect(destructivelyUpdateDriverWithKeyAndValue(driver, 'address', '12 Broadway')).to.eql({
-//       name: 'Sam',
-//       address: '12 Broadway'
-//     });
-//
-//     expect(driver).to.eql({
-//       name: 'Sam',
-//       address: '12 Broadway'
-//     });
-//   });
-// });
+
 //
 // describe('deleteFromDriverByKey(driver, key)', function () {
 //   it('deletes `key` from a clone of driver and returns the new driver (it is non-destructive)', function () {
