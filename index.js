149d700aceda085d8b6933ecd953c3406bdb7b1d
Hoon Lee <yl.hoony@gmail.com>
2017-09-30 19:14:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   return newDriver;
 }
 
-function destructivelyUpdatedriverWithKeyAndValue(driver, key, value) {
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
   return driver;
 }
